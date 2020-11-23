feb6d19212c0f36d53286320857ac754e6705e0d
JesseStuy20 <jxie20@stuy.edu>
2020-11-23 15:23:12 -0500

    Added private variable, constructor, basic methods, equals method, and tester file with test cases

---
+++ NumTester.java
@@ -0,0 +1,17 @@
+public class NumTester {
+
+  public static void main(String[] args) {
+    RealNumber a = new RealNumber(5.9) ;
+    RealNumber b = new RealNumber(5.9) ;
+    RealNumber c = new RealNumber(5.912) ;
+    RealNumber d = new RealNumber(0.0) ;
+    RealNumber e = new RealNumber(0.000000000000001) ;
+    System.out.println(a.toString()) ;
+    System.out.println(a.equals(b)) ;
+    System.out.println(a.equals(c)) ;
+    System.out.println(d.equals(a)) ;
+    System.out.println(d.equals(e)) ;
+    System.out.println(d.equals(d)) ;
+  }
+
+}
---
+++ RealNumber.java
@@ -0,0 +1,29 @@
+public class RealNumber {
+  private double value;
+
+  public RealNumber(double v) {
+    value = v ;
+  }
+
+  public double getValue() {
+    return value ;
+  }
+
+  public String toString() {
+    return getValue() + "" ;
+  }
+
+  public boolean equals(RealNumber other){
+    boolean result = false ;
+    if ((getValue() == 0.0) && (other.getValue() == 0.0)) {
+      result = true ;
+    }
+    if ((getValue() != 0.0) && (other.getValue() != 0.0)) {
+      if (Math.abs(getValue() - other.getValue()) < 0.00001) {
+        result = true ;
+      }
+    }
+    return result ;
+  }
+
+}
