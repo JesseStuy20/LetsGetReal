0bb487b0c9a3dd6ffbccaaf321c7035b284e3476
JesseStuy20 <jxie20@stuy.edu>
2020-11-26 16:29:16 -0500

    Moved equals method to number and added compareTo method

--- Number.java
+++ Number.java
@@ -2,4 +2,25 @@ public abstract class Number {
 
     public abstract double getValue() ;
 
+    public boolean equals(RealNumber other) {
+      boolean result = false ;
+      if ((getValue() == 0.0) && (other.getValue() == 0.0)) {
+        result = true ;
+      }
+      if ((getValue() != 0.0) && (other.getValue() != 0.0)) {
+        if (Math.abs(getValue() - other.getValue()) < 0.00001) {
+          result = true ;
+        }
+      }
+      return result ;
+    }
+
+    public int compareTo(Number other) {
+      int result = 0 ;
+      if (this.getValue() == other.getValue()) result = 0 ;
+      if (this.getValue() > other.getValue()) result = 1 ;
+      if (this.getValue() < other.getValue()) result = -1 ;
+      return result ;
+  }
+
 }
--- RealNumber.java
+++ RealNumber.java
@@ -13,19 +13,6 @@ public class RealNumber extends Number {
     return getValue() + "" ;
   }
 
-  public boolean equals(RealNumber other) {
-    boolean result = false ;
-    if ((getValue() == 0.0) && (other.getValue() == 0.0)) {
-      result = true ;
-    }
-    if ((getValue() != 0.0) && (other.getValue() != 0.0)) {
-      if (Math.abs(getValue() - other.getValue()) < 0.00001) {
-        result = true ;
-      }
-    }
-    return result ;
-  }
-
   public RealNumber add(RealNumber other) {
     RealNumber result = new RealNumber(this.getValue() + other.getValue()) ;
     return result ;
